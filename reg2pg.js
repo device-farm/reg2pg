8078cf96545aad191bf8720092c7a7cbf0e10e7a
Pavel Burgr <pb@drake.cz>
2019-03-28 01:48:35 +0100

    WIP

--- reg2pg.js
+++ reg2pg.js
@@ -11,44 +11,39 @@ require("@device.farm/appglue")({ require, file: __dirname + "/config.json" }).m
     await db.connect();
     console.info("Connected to PostgreSQL");
 
-    let rcvValues = {};
     let regs = {};
+    let last = {};
 
     mqttReg.mqttAdvertise(config.mqtt, name => {
         if (!regs[name]) {
             console.info("Adding register", name);
-            regs[name] = mqttReg.mqttReg(config.mqtt, name, (value, prev, initial) => {
-                if (!initial) {
-                    if (typeof value === "number") {
-                        value = Math.round(value * 2) / 2;
-                    }
-                    rcvValues[name] = value;
-                }
-            });
+            regs[name] = mqttReg.mqttReg(config.mqtt, name, (value, prev, initial) => {});
         }
     });
 
-    let sentValues = {};
     let working = false;
 
     setInterval(async () => {
         try {
             if (!working) {
                 working = true;
-                for ([name, value] of Object.entries(rcvValues)) {
+                for ([name, reg] of Object.entries(regs)) {
+
+                    let value = reg.actual();
 
                     if (typeof value === "boolean") {
                         value = value ? 1 : 0;
                     }
 
-                    if ((typeof value === "number" || value === undefined) && sentValues[name] !== value) {
+                    if ((typeof value === "number") || ((value === undefined) && (last[name] !== undefined))) {
 
-                        sentValues[name] = value;
                         try {
                             await db.query("insert into regs(time, id_site, reg, value) values(current_timestamp, $1, $2, $3)", [siteId, name, value]);
                         } catch (e) {
                             console.error(`Error inserting ${value} for register ${name} of site ${siteId}:`, e.message || e);
                         }
+
+                        last[name] = value;
 
                     }
                 };
@@ -56,6 +51,6 @@ require("@device.farm/appglue")({ require, file: __dirname + "/config.json" }).m
         } finally {
             working = false;
         }
-    }, 2000);
+    }, config.intervalMs);
 
 });
